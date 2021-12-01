4b64e5f73dcb970a31a255104c2d3536c13b2d11
Asaf Cohen <safhac@gamil.com>
2021-12-01 13:54:40 +0200

    added MyTweepAPI class as facade

--- crypto_twitter_sentiment_tracker_exercise.py
+++ crypto_twitter_sentiment_tracker_exercise.py
@@ -1,9 +1,14 @@
 import asyncio
 import json
+from datetime import datetime, timedelta
 from urllib.request import urlopen
 import tweepy  # using version 4.1.0
 
+# UTC
+utc_now = lambda: datetime.utcnow()
+
 URL = "https://financialmodelingprep.com/api/v3/quote/BTCUSD?apikey=9c33655ac70d040280297ef04cf3ceff"
+
 # Twitter OAuth Authentication
 CONSUMER_KEY = "dqyaBG6BbC1gETauyEDyoyaib"
 CONSUMER_SECRET = "pKvBnWNT7Wxr7Z3WwQzhtJ1BlF1cKNw7LEN0597jYsSEE1cCTu"
@@ -11,37 +16,88 @@ CONSUMER_SECRET = "pKvBnWNT7Wxr7Z3WwQzhtJ1BlF1cKNw7LEN0597jYsSEE1cCTu"
 ACCESS_TOKEN = "1269644061079674888-NzGrf0MAEVl6fGDSTtpUTYJMNBezvb"
 ACCESS_TOKEN_SECRET = "ubqTldQLx7atDcXUwUlWJ8lLqx1wWkrkCKud97n6PswpN"
 
-QUERY_STRING = "$BTC" + " -filter:retweets"
+
+def save_task_results(results: str):
+    with open('task_results', 'w') as file:
+        file.writelines(results)
 
 
-async def get_json_parsed_data(url: str) -> str:
-    response = urlopen(url)
-    data = response.read().decode("utf-8")
-    return json.loads(data)
+class MyTweepyApi:
+    def __init__(self,
+                 api: tweepy.API,
+                 query: str,
+                 until: datetime,
+                 every: int) -> None:
+        """
+        class representing the task
+        :param api: the api used
+        :param query: the query for the api
+        :param delta: running time delta
+        :param every: scheduled running period
+        """
+        self.api = api
+        self.query = query
+        self.every = every
+        self.until = until
+        self.task = asyncio.create_task(self.periodic())
+
+    async def start(self):
+        await self.task
+
+    async def stop(self):
+        self.task.cancel()
+
+    async def get_json_parsed_data(self, url: str) -> str:
+        response = urlopen(url)
+        data = response.read().decode("utf-8")
+        return json.loads(data)
+
+    async def get_btc_price(self):
+        parsed_data = (await self.get_json_parsed_data(URL))[0]
+        return parsed_data["price"]
+
+    async def get_btc_tweets(self):
+        return self.api.search_tweets(
+            q=self.query,
+            result_type="recent",
+            count=100,
+            tweet_mode='extended'
+        )
+
+    async def periodic(self) -> None:
+        while utc_now() < self.until:
+            results = await asyncio.gather(
+                self.get_btc_price(),
+                self.get_btc_tweets()
+            )
+            # save results
+            save_task_results(results)
+            await asyncio.sleep(self.every)
+
+        else:
+            print('completed')
+            await self.stop()
 
 
-async def main():
+async def main(api: tweepy.API) -> None:
     print("Welcome to crypto twitter sentiment tracker")
 
-    parsed_data = (await get_json_parsed_data(URL))[0]
-    price = parsed_data["price"]
+    until = utc_now() + timedelta(hours=2)
+    every = 60  # in seconds
+    query = "$BTC" + " -filter:retweets"
 
-    print("bitcoin price: " + str(price))
+    tweepy_api = MyTweepyApi(api, query, until, every)
+    try:
+        await tweepy_api.start()
 
-    # Configure tweepy
-    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-    twitter_api = tweepy.API(auth)
-
-    tweets = twitter_api.search_tweets(
-        q=QUERY_STRING,
-        result_type="recent",
-        count=100,
-        tweet_mode='extended'
-    )
-
-    print(tweets[0])
+    except asyncio.CancelledError as e:
+        print(e, e.args)
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    # Configure tweepy
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    api = tweepy.API(auth)
+
+    asyncio.run(main(api))
