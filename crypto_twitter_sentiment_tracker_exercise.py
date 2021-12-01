c18363b0216c411ca0ce56dcd189f78ba3e1ecf9
Asaf Cohen <safhac@gamil.com>
2021-12-01 10:42:43 +0200

    basic refactoring main

--- crypto_twitter_sentiment_tracker_exercise.py
+++ crypto_twitter_sentiment_tracker_exercise.py
@@ -1,37 +1,45 @@
+import asyncio
 import json
 from urllib.request import urlopen
 import tweepy  # using version 4.1.0
 
+URL = "https://financialmodelingprep.com/api/v3/quote/BTCUSD?apikey=9c33655ac70d040280297ef04cf3ceff"
+# Twitter OAuth Authentication
+CONSUMER_KEY = "dqyaBG6BbC1gETauyEDyoyaib"
+CONSUMER_SECRET = "pKvBnWNT7Wxr7Z3WwQzhtJ1BlF1cKNw7LEN0597jYsSEE1cCTu"
 
-def get_jsonparsed_data(url):
+ACCESS_TOKEN = "1269644061079674888-NzGrf0MAEVl6fGDSTtpUTYJMNBezvb"
+ACCESS_TOKEN_SECRET = "ubqTldQLx7atDcXUwUlWJ8lLqx1wWkrkCKud97n6PswpN"
+
+QUERY_STRING = "$BTC" + " -filter:retweets"
+
+
+def get_json_parsed_data(url: str) -> str:
     response = urlopen(url)
     data = response.read().decode("utf-8")
     return json.loads(data)
 
 
-if __name__ == "__main__":
 
+async def main():
     print("Welcome to crypto twitter sentiment tracker")
 
-    url = "https://financialmodelingprep.com/api/v3/quote/BTCUSD?apikey=9c33655ac70d040280297ef04cf3ceff"
-    parsed_data = get_jsonparsed_data(url)[0]
+    parsed_data = get_json_parsed_data(URL)[0]
     price = parsed_data["price"]
 
     print("bitcoin price: " + str(price))
 
-    # Twitter OAuth Authentication
-    consumer_key = "dqyaBG6BbC1gETauyEDyoyaib"
-    consumer_secret = "pKvBnWNT7Wxr7Z3WwQzhtJ1BlF1cKNw7LEN0597jYsSEE1cCTu"
-
-    access_token = "1269644061079674888-NzGrf0MAEVl6fGDSTtpUTYJMNBezvb"
-    access_token_secret = "ubqTldQLx7atDcXUwUlWJ8lLqx1wWkrkCKud97n6PswpN"
-
     # Configure tweepy
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
     twitter_api = tweepy.API(auth)
 
-    query_string = "$BTC" + " -filter:retweets"
-
-    tweets = twitter_api.search_tweets(q=query_string, result_type="recent", count=100, tweet_mode='extended')
+    tweets = twitter_api.search_tweets(q=QUERY_STRING, result_type="recent", count=100, tweet_mode='extended')
     print(tweets[0])
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+
+
