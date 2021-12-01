0d5203b0c2b6f5f5afca13382d27c4abb87abfaa
Asaf Cohen <safhac@gamil.com>
2021-12-01 10:50:29 +0200

    change function to be async

--- crypto_twitter_sentiment_tracker_exercise.py
+++ crypto_twitter_sentiment_tracker_exercise.py
@@ -17,7 +17,7 @@ QUERY_STRING = "$BTC" + " -filter:retweets"
 async def get_json_parsed_data(url: str) -> str:
     response = urlopen(url)
     data = response.read().decode("utf-8")
-    return json.loads(data)
+    yield json.loads(data)
 
 
 async def main():
